daa2730fe12e299259b42b859fd26cca9d9e2bed
Arun S <sarunb4u@gmail.com>
2022-03-31 22:33:28 -0400

    Fruit basket problem

---
+++ Sliding Window Pattern/Fruits into Baskets/FruitBasket.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+
+public class FruitBasket {
+    // O(N) time | O(K) space
+    // N is the length of the string and K is the dinstinct chars
+    public static int findLength(char[] tree) {
+        // base checks
+        if (tree.length == 0 || tree.length < 2) {
+            throw new IllegalArgumentException();
+        }
+
+        HashMap<Character, Integer> fruitFrequencyMap = new HashMap<>();
+        int windowStart = 0, maxLength = 0;
+
+        for (int windowEnd = 0; windowEnd < tree.length; windowEnd++) {
+            char nextTree = tree[windowEnd];
+
+            // add next char to hash map and increase the char count
+            fruitFrequencyMap.put(nextTree, fruitFrequencyMap.getOrDefault(nextTree, 0) + 1);
+
+            while (fruitFrequencyMap.size() > 2) {
+                char leftTree = tree[windowStart];
+
+                // decrease the char count of the left most char
+                fruitFrequencyMap.put(leftTree, fruitFrequencyMap.get(leftTree) - 1);
+
+                // if count is zero, remove it from hashmap
+                if (fruitFrequencyMap.get(leftTree) == 0) {
+                    fruitFrequencyMap.remove(leftTree);
+                }
+
+                // slide the window
+                windowStart++;
+            }
+
+            // window length = (windowEnd - windowStart) + 1
+            maxLength = Math.max(maxLength, (windowEnd - windowStart) + 1);
+        }
+
+        System.out.println(fruitFrequencyMap.toString());
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out
+                .println("Maximum number of fruits: " + FruitBasket.findLength(new char[] { 'A', 'B', 'C', 'A', 'C' }));
+        System.out.println(
+                "Maximum number of fruits: " + FruitBasket.findLength(new char[] { 'A', 'B', 'C', 'B', 'B', 'C' }));
+    }
+}
--- Sliding Window Pattern/Longest Substring with K Distinct Characters/LongestSubstringKDistinct.java
+++ Sliding Window Pattern/Longest Substring with K Distinct Characters/LongestSubstringKDistinct.java
@@ -1,7 +1,8 @@
 import java.util.HashMap;
 
 public class LongestSubstringKDistinct {
-    //
+    // O(N) time | O(K) space
+    // N is the length of the string and K is the dinstinct chars
     public static int findLength(String str, int k) {
         // base checks
         if (str == null || str.length() == 0 || str.length() < k) {
@@ -14,25 +15,25 @@ public class LongestSubstringKDistinct {
         for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
             char nextChar = str.charAt(windowEnd);
 
-            //add next char to hash map and increase the char count
+            // add next char to hash map and increase the char count
             charFrequency.put(nextChar, charFrequency.getOrDefault(nextChar, 0) + 1);
 
             while (charFrequency.size() > k) {
                 char leftChar = str.charAt(windowStart);
 
-                //decrease the char count of the left most char
+                // decrease the char count of the left most char
                 charFrequency.put(leftChar, charFrequency.get(leftChar) - 1);
 
-                //if count is zero, remove it from hashmap
+                // if count is zero, remove it from hashmap
                 if (charFrequency.get(leftChar) == 0) {
                     charFrequency.remove(leftChar);
                 }
 
-                //slide the window
+                // slide the window
                 windowStart++;
             }
 
-            //window length = (windowEnd - windowStart) + 1
+            // window length = (windowEnd - windowStart) + 1
             maxLength = Math.max(maxLength, (windowEnd - windowStart) + 1);
         }
 
