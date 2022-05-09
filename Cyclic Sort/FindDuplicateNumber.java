3af565442179683cb4ce62425c811ddcea5331fa
Arun S <sarunb4u@gmail.com>
2022-05-08 22:36:00 -0400

    find duplicate number problem

--- Cyclic Sort/FindDuplicateNumber.java
+++ Cyclic Sort/FindDuplicateNumber.java
@@ -1,5 +1,8 @@
 public class FindDuplicateNumber {
-    //
+    // O(n) time | O(1) space
+    // where n is the length of the array
+    // time: O(n)+O(n−1)+O(n) i.e. O(n) for while loop, O(n-1) for swapping and O(n)
+    // for for loop    
     public static int findNumber(int[] nums) {
         // base checks
         if (nums == null || nums.length == 0) {
@@ -28,27 +31,31 @@ public class FindDuplicateNumber {
         arr[j] = temp;
     }
 
+    // O(n) time | O(1) space
+    // where n is the length of the array
     public static int findNumberTwoPointers(int[] nums) {
         int slow = 0, fast = 0;
-        do{
+        do {
             slow = nums[slow];
             fast = nums[nums[fast]];
-        }while(slow != fast);
+        } while (slow != fast);
+
         slow = 0;
-        while(slow != fast){
+        while (slow != fast) {
             slow = nums[slow];
             fast = nums[fast];
         }
+
         return slow;
     }
 
     public static void main(String[] args) {
         System.out.println(FindDuplicateNumber.findNumber(new int[] { 1, 4, 4, 3, 2 }));
         System.out.println(FindDuplicateNumber.findNumber(new int[] { 2, 1, 3, 3, 5, 4 }));
-        System.out.println(FindDuplicateNumber.findNumber(new int[] { 2, 4, 1, 4, 4 }));
+        System.out.println(FindDuplicateNumber.findNumber(new int[] { 1, 3, 4, 2, 2 }));
 
         System.out.println(FindDuplicateNumber.findNumberTwoPointers(new int[] { 1, 4, 4, 3, 2 }));
         System.out.println(FindDuplicateNumber.findNumberTwoPointers(new int[] { 2, 1, 3, 3, 5, 4 }));
-        System.out.println(FindDuplicateNumber.findNumberTwoPointers(new int[] { 2, 4, 1, 4, 4 }));
+        System.out.println(FindDuplicateNumber.findNumberTwoPointers(new int[] { 1, 3, 4, 2, 2 }));
     }
 }
